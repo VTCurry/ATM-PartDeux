93b53eea136e740a86bc6b8c0616d8cba8e14264
vtcurry <vtcurry@gmail.com>
2017-02-16 18:38:22 -0500

    first Try

--- ATM part 2/src/com/novauc/Main.java
+++ ATM part 2/src/com/novauc/Main.java
@@ -1,58 +1,53 @@
 package com.novauc;
 
 import java.util.Scanner;
+        import java.util.Scanner;
 
 public class Main {
 
+    public static void main(String[] args) throws Exception{
+        // public void choosename (){
+        System.out.println("Welcome to Illuminati ATM");
+        System.out.println("$$$$$$$$$$$$$$$$$$$$$$$$$$$$");
+        System.out.println("$ Illuminati Bank & Trust  $");
+        System.out.println("$$$$$$$$$$$$$$$$$$$$$$$$$$$$");
+        System.out.println("What is your name?");
+        Scanner scanner = new Scanner(System.in);
 
-    public static accounts accounts = new accounts();
-    public static com.novauc.accounts getAccounts() {
-    }
+        int balance = 100;
+        String name = scanner.nextLine();
+        if (name.length() == 0) {
+            throw new Exception("Not a valid name.");
+        }
 
-    public static void main(String[] args) throws Exception
-    {
-                // public void choosename (){
-                System.out.println("Welcome to Illuminati ATM");
-                System.out.println("$$$$$$$$$$$$$$$$$$$$$$$$$$$$");
-                System.out.println("$ Illuminati Bank & Trust  $");
-                System.out.println("$$$$$$$$$$$$$$$$$$$$$$$$$$$$");
-                System.out.println("What is your name?");
-                Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.println("[1] Check my balance");
+            System.out.println("[2] Withdraw Funds");
+            System.out.println("[3] Close Account");
+            System.out.println("[4] Cancel");
+            String choice = scanner.nextLine();
+            int choiceNum = Integer.valueOf(choice);
+            if (choiceNum == 1) {
+                System.out.println("Your balance is $" + balance);
 
-                String name = scanner.nextLine();
-                if (name.length() == 0) {
-                    throw new Exception("Not a valid name.");
-                } else {
-                    System.out.println("How can we help you? " + name);
+            } else if (choiceNum == 4) {
+                System.out.println("Thank you and please come again.");
+                System.exit(0);
 
-                    Double }
-                    System.out.println("[1] Check my balance");
-                    System.out.println("[2] Withdraw Funds");
-                    System.out.println("[3] Cancel");
-                    String choice = scanner.nextLine();
-                    int choiceNum = Integer.valueOf(choice);
-                    if (choiceNum == 1) {
-                        System.out.println("Your balance is $" + balance);
-
-                    } else if (choiceNum == 3) {
-                        System.out.println("Thank you and please come again.");
-                        System.exit(0);
-
-                    } else if (choiceNum == 2) {
-                        System.out.println("How much money would you like to withdraw?");
-                        String amount = scanner.nextLine();
-                        int amountNum = Integer.valueOf(amount);
-                        if (amountNum > balance) {
-                            System.out.println("ATM currently only has $" + balance);
-                        } else if (amountNum <= balance) {
-                            continue;
-                            double newBal = (balance - amountNum);
-                            accounts.put(name, newBal);
-                            System.out.println("Your balance is now $" + (balance - amountNum));
-                            System.out.println("Please take your money.");
-                        }
-
-                    }
-                }
+            } else if (choiceNum == 2) {
+                System.out.println("How much money would you like to withdraw?");
+                String amount = scanner.nextLine();
+                int amountNum = Integer.valueOf(amount);
+                if (amountNum > balance) {
+                    System.out.println("Your Account currently only has $" + balance);
+            } else if (amountNum <= balance) {
+                double newBal = (balance - amountNum);
+                System.out.println("Your balance is now $" + newBal);
+                System.out.println("Please take your money.");
             }
 
+            }
+
+        }
+    }
+}
